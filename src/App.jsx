a3f26fc5934fb7f17521dad892e7976e50fd5fff
Daniel Bonn <danieltbonn@gmail.com>
2023-11-01 20:28:35 -0500

    added basic func for a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import About from './components/pages/about';
 import Portfolio from './components/pages/portfolio';
 import Contact from './components/pages/contact';
 import Resume from './components/pages/resume';
 
+// Main App that will handle page changes and other app logic
 function App() {
+    // use state for currentPage
     const [currentPage, setCurrentPage] = useState('About');
 
+    // allows us to render a page depending on what the currentPage is set as
     const renderPage = () => {
         if (currentPage === 'About'){
             return <About />
@@ -22,12 +26,14 @@ function App() {
 
     };
 
+    // accesses currentPage useState to change it to the current desired page
     const handlePageChange = (page) => setCurrentPage(page);
 
     return (
         <div>
             <Header currentPage={currentPage} handlePageChange={handlePageChange}/>
             <main className="main-page">{renderPage()}</main>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <h2>Github Icon</h2>
+            <h2>LinkedIn Icon</h2>
+            <h2>Telephone Icon</h2>
+        </footer>
+    );
+}
