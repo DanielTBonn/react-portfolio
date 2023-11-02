3ddb72c1689f243395ee3e516cf74a031a6ae30c
Daniel Bonn <danieltbonn@gmail.com>
2023-11-01 19:41:58 -0500

    deleted unnecessary code on Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,4 @@
-// TODO: Add a comment explaining how we are able to extract the key value pairs from props
-
-
-// const pageMap = pages.map((page) => {
-//   <li className="nav-item">
-//         <a
-//           href={`#${page}`}
-//           onClick={() => handlePageChange({page})}
-//           //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
-
-//           className={currentPage === `${page}` ? 'nav-link active' : 'nav-link'}
-//         >
-//           Home
-//         </a>
-//       </li>
-// })
-
+// Create navigation tabs to be applied to a header
 function Navigation({ currentPage, handlePageChange }) {
   const pages = ['About', 'Portfolio', 'Contact', 'Resume']
 
@@ -33,50 +17,6 @@ function Navigation({ currentPage, handlePageChange }) {
               </a>
             </li>
       ))}
-      {/* <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
-
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          //  TODO: Add a comment explaining what this logic is doing
-
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
-          //  TODO: Add a comment explaining what this logic is doing
-
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
-        >
-          Blog
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          //  TODO: Add a comment explaining what this logic is doing
-
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li> */}
     </ul>
   );
 }
