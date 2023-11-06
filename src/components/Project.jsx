5d593cb93290dd7fcd59ef091cba69d69f6bc362
Daniel Bonn <danieltbonn@gmail.com>
2023-11-05 20:24:21 -0600

    added images, styling, and links to the portfolio section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,14 +1,35 @@
 export default function Project() {
-    const items = ['Item 1', 'Item 2', 'Item 3']
+    const projects = [
+    { 
+        image: '/public/assets/images/techstop-website.png',
+        link: 'https://github.com/DanielTBonn/techstop-ecommerce'
+    }, 
+    {
+        image: '/public/assets/images/MVC-tech-blog.png',
+        link: 'https://github.com/DanielTBonn/tech-blog'
+    }, 
+    { 
+        image: '/public/assets/images/employee-tracker-pic.png',
+        link: 'https://github.com/DanielTBonn/employee-tracker'
+    }, 
+    { 
+        image: '/public/assets/images/weather-application-pic.png',
+        link: 'https://github.com/DanielTBonn/weather-application'
+    }, 
+    { 
+        image: '/public/assets/images/joke-gen-webpage.png',
+        link: 'https://github.com/DanielTBonn/joke-generator'
+    }
+]
 
     return ( 
-        <div className="project-item">
-            <div className="project-container">
+        <div className="project-item mx-auto max-w-5xl">
+            <div className="project-container grid grid-cols-2 grid-rows-3 gap-5">
+                {projects.map((project) => (
                 <div className="project-image">
-                {items.map((item) => (
-                    <p>{item}</p>
-                ))}    
+                    <a href={project.link} target="_blank"><img src={project.image}></img></a>
                 </div>
+                ))}    
             </div>
         </div>
     );
