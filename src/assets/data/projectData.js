0922d80665d7bf77789bd2a5e8f49e3a21dd4edb
Daniel Bonn <danieltbonn@gmail.com>
2023-11-14 20:15:29 -0600

    changed the project component to be reuseable and moved data to a different folder

---
+++ src/assets/data/projectData.js
@@ -0,0 +1,40 @@
+export const projects = [
+    {   
+        name: 'techstop',
+        image: '/assets/images/techstop-website.png',
+        link: 'https://github.com/DanielTBonn/techstop-ecommerce',
+        deployed: 'https://pacific-fortress-50526-9193db3a3754.herokuapp.com/'
+    }, 
+    {
+        name: 'techblog',
+        image: '/assets/images/MVC-tech-blog.png',
+        link: 'https://github.com/DanielTBonn/tech-blog',
+        deployed: 'https://gentle-retreat-99596-ab9698174a1f.herokuapp.com/'
+    },
+    {
+        name: 'jate',
+        image: '/assets/images/jate.png',
+        link: 'https://github.com/DanielTBonn/pwa-text-editor',
+        deployed: 'https://damp-peak-19637-7250a967b51c.herokuapp.com/'
+    },  
+    { 
+        name: 'employee-tracker',
+        image: '/assets/images/employee-tracker-pic.png',
+        link: 'https://github.com/DanielTBonn/employee-tracker',
+        deployed: 'https://github.com/DanielTBonn/employee-tracker'
+    }, 
+    { 
+        name: 'weather-app',
+        image: '/assets/images/weather-application-pic.png',
+        link: 'https://github.com/DanielTBonn/weather-application',
+        deployed: 'https://danieltbonn.github.io/weather-application/'
+    }, 
+    { 
+        name: 'joke-gen',
+        image: '/assets/images/joke-gen-webpage.png',
+        link: 'https://github.com/DanielTBonn/joke-generator',
+        deployed: 'https://danieltbonn.github.io/joke-generator/'
+    }
+]
+
+// module.exports = projects;
--- src/components/pages/portfolio.jsx
+++ src/components/pages/portfolio.jsx
@@ -1,4 +1,5 @@
 import Project from '../Project';
+import { projects } from '../../assets/data/projectData';
 
 export default function Portfolio() {
 
@@ -6,7 +7,15 @@ export default function Portfolio() {
         <div className="portfolio-section px-6 py-24 sm:py-32 lg:px-8">
             <h1 className="portfolio-header text-3xl font-bold mx-auto max-w-6xl">Portfolio</h1>
             <div className="portfolio-body">
-                    <Project />
+                <div className="project-item mx-auto max-w-5xl mt-10">
+                    <div className="project-container grid grid-cols-2 grid-rows-3 gap-5">
+                    {projects.map((project) => ( 
+                        <div className="project-image" key={project.name}>
+                            <Project project={project} /> 
+                        </div>
+                        ))} 
+                    </div>
+                </div>
             </div>    
         </div>
     );
